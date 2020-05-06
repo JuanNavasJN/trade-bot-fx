33f5304b9db9a7c30f8aa805e770d9474f13ee20
Juan Navas <navascruzf@gmail.com>
2020-05-06 18:04:52 -0400

    update

--- src/components/Home/Header/index.js
+++ src/components/Home/Header/index.js
@@ -5,12 +5,12 @@ import './index.css';
 const Header = _ => {
     return (
         <header>
-            {/* <h1>Pandapips</h1> */}
-            <div className="cards-container">
+            <h1>Pandapips</h1>
+            {/* <div className="cards-container">
                 <div className="card"></div>
                 <div className="card center"></div>
                 <div className="card"></div>
-            </div>
+            </div> */}
         </header>
     );
 };
