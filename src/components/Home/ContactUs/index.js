f68cca2b0625758d9476d43159fa293de6d54956
Victor H Montoya R <victor.montoya8@gmail.com>
2020-05-07 11:04:55 -0400

    modificaciones en la seccion3 y otros

--- src/components/Home/ContactUs/index.js
+++ src/components/Home/ContactUs/index.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import { AwesomeButton } from "react-awesome-button";
+import "./index.css";
 
-const ContactUs = _ => {
-    return (
-        <section className="contact-us" id="contact-us">
-            <div>
-                <h2>Contact Us</h2>
-            </div>
-            <div className="form-container">
-                <form>
-                    <input type="text" placeholder="Name" />
-                    <input type="email" placeholder="E-mail" />
-                    <input type="text" placeholder="Message" />
-                </form>
-            </div>
-        </section>
-    );
+const ContactUs = (_) => {
+  return (
+    <section className="contact-us" id="contact-us">
+      <div>
+        <h2>Contact Us</h2>
+      </div>
+      <div className="form-container">
+        <form>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="E-mail" />
+          <input type="text" placeholder="Message" />
+          <AwesomeButton type="primary">Send</AwesomeButton>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default ContactUs;
--- src/components/Home/Section3/index.js
+++ src/components/Home/Section3/index.js
@@ -1,32 +1,79 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import Swiper from "react-id-swiper";
+import "swiper/css/swiper.css";
+import "./index.css";
+import pr1 from "../../../assets/img/p1.jpg";
+import pr2 from "../../../assets/img/p2.jpeg";
+import pr3 from "../../../assets/img/p3.jpeg";
 
-const Section1 = _ => {
-    return (
-        <section className="section-3">
-            <div className="title-container">
-                <h2>Title Section 3</h2>
+const Section1 = (_) => {
+  const params = {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  };
+
+  return (
+    <section className="section-3">
+      <div className="title-container">
+        <h2>See what other people are saying about GPS FOREX ROBOT</h2>
+      </div>
+      <div className="section-body">
+        <div className="section-wrapper">
+          <Swiper {...params}>
+            <div>
+              <p className="text-content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+                excepturi commodi sed perspiciatis, voluptatum doloribus porro
+                odio eos recusandae animi? Aliquam obcaecati tempora enim ipsa
+                repellendus magni nulla nihil architecto?
+              </p>
             </div>
-            <div className="section-body">
-                <div className="section-wrapper">
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Esse excepturi commodi sed perspiciatis, voluptatum
-                        doloribus porro odio eos recusandae animi? Aliquam
-                        obcaecati tempora enim ipsa repellendus magni nulla
-                        nihil architecto?
-                    </p>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Esse excepturi commodi sed perspiciatis, voluptatum
-                        doloribus porro odio eos recusandae animi? Aliquam
-                        obcaecati tempora enim ipsa repellendus magni nulla
-                        nihil architecto?
-                    </p>
-                </div>
+            <div>
+              <p className="text-content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+                excepturi commodi sed perspiciatis, voluptatum doloribus porro
+                odio eos recusandae animi? Aliquam obcaecati tempora enim ipsa
+                repellendus magni nulla nihil architecto?
+              </p>
             </div>
-        </section>
-    );
+            <div>
+              <p className="text-content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+                excepturi commodi sed perspiciatis, voluptatum doloribus porro
+                odio eos recusandae animi? Aliquam obcaecati tempora enim ipsa
+                repellendus magni nulla nihil architecto?
+              </p>
+            </div>
+          </Swiper>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+            excepturi commodi sed perspiciatis, voluptatum doloribus porro odio
+            eos recusandae animi? Aliquam obcaecati tempora enim ipsa
+            repellendus magni nulla nihil architecto?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+            excepturi commodi sed perspiciatis, voluptatum doloribus porro odio
+            eos recusandae animi? Aliquam obcaecati tempora enim ipsa
+            repellendus magni nulla nihil architecto?
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Section1;
