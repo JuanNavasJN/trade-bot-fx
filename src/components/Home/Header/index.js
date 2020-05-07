1eb85107792981a7e94e28d5835bc0c3ec47aa3b
Juan Navas <navascruzf@gmail.com>
2020-05-07 10:00:00 -0400

    update header

--- src/components/Home/Header/index.js
+++ src/components/Home/Header/index.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React from 'react';
 
-import "./index.css";
+import './index.css';
 
-const Header = (_) => {
-  return (
-    <header>
-      <h1>Pandapips</h1>
-      {/* <div className="cards-container">
-                <div className="card"></div>
-                <div className="card center"></div>
-                <div className="card"></div>
-            </div> */}
-    </header>
-  );
+const Header = _ => {
+    return (
+        <header>
+            <div>
+                <h1>Pandapips</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. A,
+                    at deleniti! Quidem odit, ullam officia tenetur ipsa, dolor
+                    ipsum ut corporis nulla veniam dolorum, itaque aperiam
+                    dolore molestiae officiis quam!
+                </p>
+            </div>
+            <div>
+                <iframe
+                    // width="560"
+                    // height="315"
+                    src="https://www.youtube.com/embed/nmgKKSw5Wwo"
+                    frameborder="0"
+                    title="video"
+                    // allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    // allowfullscreen
+                ></iframe>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
