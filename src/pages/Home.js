60f7768baa3ab336566a2c0b0c0710bf70d7cf0a
Juan Navas <navascruzf@gmail.com>
2020-05-07 10:27:30 -0400

    added contactUs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import Header from '../components/Home/Header';
 import Section1 from '../components/Home/Section1';
 import Section2 from '../components/Home/Section2';
 import Section3 from '../components/Home/Section3';
+import ContactUs from '../components/Home/ContactUs';
 
 import './Home.css';
 
@@ -16,6 +17,7 @@ const Home = _ => {
                 <Section1 />
                 <Section2 />
                 <Section3 />
+                <ContactUs />
             </div>
         </div>
     );
